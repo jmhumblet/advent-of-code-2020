fe08eeacbc78a9c26bf0354b5fc8c4a4fb56f47b
JiM <jmhumblet@gmail.com>
2020-12-07 21:47:04 +0100

    mob next [ci-skip]

---
+++ AdventOfCode2020/Day1.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode2020
+{
+    public class Day1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = Find(new[] { 2018, 2 });
+            result.Should().Be(4036);
+        }
+
+        private int Find(IEnumerable<int> numbers)
+        {
+            return 0;
+        }
+    }
+}
